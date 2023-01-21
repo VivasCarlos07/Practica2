2dfeff8b4cb3c767f76d18798087b11f3bd1ea2b
Carlos Vivas <carlosvivasdev@gmail.com>
2023-01-20 23:34:52 -0500

    entrada

---
+++ Views/Shared/_Topbar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Practica2.Views.Shared
+{
+    public class _TopbarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
